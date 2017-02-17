d97bb0f1ff535f545bbe80717268aa8f002333ec
ard71 <ard71@pitt.edu>
2017-02-17 13:21:46 -0500

    Created file tree

---
+++ src/main/java/VotingSoftware.java
@@ -0,0 +1,12 @@
+public class VotingSoftware{
+	private VoterTable vt;
+	private TallyTable tt;
+	
+	public static main(String[] args){
+	}
+	public static void init(){
+		
+	}
+	public static void terminate(){
+	}
+}
