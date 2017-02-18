17c4d81a4395e24ee0b4977ef8e19e0971a54f53
arh95 <arh95@pitt.edu>
2017-02-18 11:00:11 -0500

    Add files via upload
    template for driver function to test xml message parsing

---
+++ src/main/java/XMLParseDriver.java
@@ -0,0 +1,32 @@
+
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+/**
+ *
+ * @author adamhayes
+ */
+public class XMLParseDriver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //driver fnctn, read in message created by 
+       //remote control, parse the info based on 
+       //what we want
+       
+    }
+    /*
+        MAIN FUNCTION OF DRIVER
+            purpose is to parse an XML message 
+            sent by the remote control and separate out
+            the 
+    
+    */
+    public XMLMessage parseMessage(){
+        XMLMessage parsed = new XMLMessage();
+        return parsed;
+    }
+    
+}
