e0f50a98bb21ed9b673c2d74017ae53d17756216
arh95 <arh95@pitt.edu>
2017-02-20 10:57:39 -0500

    Added comments

--- src/main/java/XMLParseDriver.java
+++ src/main/java/XMLParseDriver.java
@@ -34,14 +34,20 @@ public class XMLParseDriver {
     */
     public static XMLMessage parseMessage(String fileName){
         try{
-            System.out.println("Checkpoint 1");
+           //create a document builder object to iterate through nodes
+           //of xml file
             DocumentBuilder builder = (DocumentBuilderFactory.newInstance()).newDocumentBuilder();
-            System.out.println("Checkpoint 2");
+            
             Document xml = builder.parse(fileName);
             Element root = xml.getDocumentElement();
+            //find the phone number and voter ID that should be stored
+            //in this xml file
             String[] values = findValues(root);
-            
-            System.out.println("Checkpoint 7");
+            //return null if the values aren't present OR if an error occurs
+            if(values == null){
+                System.out.println("Invalid message type, null returned");
+                return null;
+            }
             return new XMLMessage(values[0], values[1]);
             
         }catch(Exception e){
@@ -50,50 +56,67 @@ public class XMLParseDriver {
         }
     }
     private static Element findNode(Element el, String name){
+        //find a node based on a given parent element and 
+        //the desired name of the node
+        Element e;
         NodeList nodes = el.getChildNodes();
         if(nodes != null){
+            //if the node has a child node
             for(int i = 0; i < nodes.getLength(); i++){
+                //iterate through the element nodes of the child node list
                 if(nodes.item(i).getNodeType() == Node.ELEMENT_NODE) {
-                    Element e = (Element) nodes.item(i);
+                    //get element reference to node
+                     e = (Element) nodes.item(i);
+                     //if its the node we want ,return its element reference
                     if(e.getNodeName().equals(name)){
                         return e;
                     }
                 }
             }
         } else {
-            System.out.println("Specified element not found");
+            //node list was empty
+            System.out.println("No child nodes");
         }
+        System.out.println("Specified node not found");
+        //if the node we wanted was not found
         return null;
     }
   
     private static String[] findValues(Element root){
         String[] keys = new String[2];
         Element body = findNode(root, "Body");
-        System.out.println("STEP");
+        Element e;
+        //get the items under the body node
         NodeList items = body.getChildNodes();
         boolean phoneFound = false, idFound = false;
-        System.out.println("STEP");
+        //init bool flags
         if(items != null){
+            //if there are child nodes
             for(int i = 0; i < items.getLength(); i++){
-                System.out.println("STEP");
+               //iterate through child nodes
                 if(items.item(i).getNodeType() == Node.ELEMENT_NODE){
-                    
-                    Element e = (Element) items.item(i);
+                    //if the node is the correct type
+                    e = (Element) items.item(i);
                     if(e.getNodeName().equals("Item")){
+                        //The key value pairs are listed under Item
                         Node child;
                         NodeList children = e.getChildNodes();
+                        //get node list of the item
                         for (int j = 0; j < children.getLength(); j++) {
+                            //if the current child node content is the voter phone no.
                             child = children.item(j);
+                            
                             if(child.getTextContent().equals("VoterPhoneNo")){
-                                System.out.println("PHONE");
+                                //the value is stored 2 away in the child node list,
+                                //so grab that value and store it in key[0]
                                 keys[0] = children.item(j+2).getTextContent();
-                                System.out.println("PHONE");
+                               //set phone found flag to true
                                 phoneFound = true;
                            }
                             if(child.getTextContent().equals("CandidateID")){
-                                System.out.println("ID");
+                                //same context as above
                                 keys[1] = children.item(j+2).getTextContent();
-                                System.out.println("ID");
+                                //set id found flag to true
                                 idFound = true;
                            }
                         }
@@ -103,7 +126,13 @@ public class XMLParseDriver {
                 }
             }
         }
-       
-       return keys;
+        //if both values were found, return the array
+       if(phoneFound && idFound){
+           return keys;
+       } else {
+           //else, return null to signify one or both were not present
+           return null;
+       }
     }
 }
+
